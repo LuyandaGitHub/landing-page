84a1370a1934f46fe845926ce0723b7186915e82
LuyandaGitHub <49154286+LuyandaGitHub@users.noreply.github.com>
2020-08-19 12:22:14 +0200

    zaio clone complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,30 @@
+window.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    })
+})
+
 window.addEventListener('scroll', () => {
     const desktopNav = document.querySelector('.desktop-nav');
     desktopNav.classList.toggle('scrolled', window.scrollY + 0);
 
     const mobileNav = document.querySelector('.mobile-nav');
     mobileNav.classList.toggle('scrolled', window.scrollY + 0);
+
+    if(desktopNav.classList.contains('scrolled') || mobileNav.classList.contains('scrolled')) {
+        desktopNav.querySelector('.logo').src = 'images/zaio_blue_logo.png';
+        mobileNav.querySelector('.logo').src = 'images/zaio_blue_logo.png';
+    } else {
+        desktopNav.querySelector('.logo').src = 'images/zaio_white_logo.png';
+        mobileNav.querySelector('.logo').src = 'images/zaio_white_logo.png';
+    }
+})
+
+
+document.querySelector('.menu-icon-container').addEventListener('click', () => {
+    console.log('click');
+    document.querySelector('.menu-icon-container').classList.toggle('active');
+    document.querySelector('.mobile-menu').classList.toggle('active');
 })
