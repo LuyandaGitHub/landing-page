42960c3ca42793f9856849c435374c0d32acc7e8
LuyandaGitHub <49154286+LuyandaGitHub@users.noreply.github.com>
2020-08-20 00:52:12 +0200

    added toggle .active class on click of mobile menu items

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// INITIALISE AOS FOR THE FADE IN ANIMATIONS 
 window.addEventListener('DOMContentLoaded', () => {
     AOS.init({
         easing: 'ease',
@@ -6,6 +7,7 @@ window.addEventListener('DOMContentLoaded', () => {
     })
 })
 
+// TOGGLE THE .scrolled CLASS ON THE desktopNav AND mobileNav
 window.addEventListener('scroll', () => {
     const desktopNav = document.querySelector('.desktop-nav');
     desktopNav.classList.toggle('scrolled', window.scrollY + 0);
@@ -13,6 +15,7 @@ window.addEventListener('scroll', () => {
     const mobileNav = document.querySelector('.mobile-nav');
     mobileNav.classList.toggle('scrolled', window.scrollY + 0);
 
+    // CHANGE THE IMAGE TO THE APPROPRIATE ONE DEPENDDING ON THE desktopNav AND mobileNav STATES
     if(desktopNav.classList.contains('scrolled') || mobileNav.classList.contains('scrolled')) {
         desktopNav.querySelector('.logo').src = 'images/zaio_blue_logo.png';
         mobileNav.querySelector('.logo').src = 'images/zaio_blue_logo.png';
@@ -22,9 +25,17 @@ window.addEventListener('scroll', () => {
     }
 })
 
-
+// TOGGLE THE .active CLASS ON THE MENU ICON AND THE MOBILE MENU
 document.querySelector('.menu-icon-container').addEventListener('click', () => {
-    console.log('click');
     document.querySelector('.menu-icon-container').classList.toggle('active');
     document.querySelector('.mobile-menu').classList.toggle('active');
+})
+
+// CREATE AN ARRAY OF navLinks TO TOGGLE THE .active CLASS ON THE MENU ICON AND THE MOBILE MENU
+const navLinks = document.querySelectorAll('.mobile-menu a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.menu-icon-container').classList.toggle('active');
+        document.querySelector('.mobile-menu').classList.toggle('active');
+    })
 })
